f769fc5863c91f48cb0a91f749a86979ef19cd85
jedidiahmarada <you@example.com>
2021-10-25 21:04:26 +0700

    selesaikan comment

--- form-danamas/src/App.js
+++ form-danamas/src/App.js
@@ -43,11 +43,15 @@ export default function App() {
   };
 
   // =================
-  //
+  // what? state untuk fungsi2 yang berjalan SETELAH tombol submit dipencet
+  // why is it important? untuk menambahkan bagian2 yang hanya muncul SETELAH tombol submit dipencet
+  // how is it important? karna fungsi2 yang berjalan SETELAH tombol submit dipencet tidak akan berjalan kalau tidak dibuat state khusus
   const [submitted, setSubmitted] = useState(false);
 
   // =================
-
+  // what? fungsi yang dibuat untuk validasi
+  // why is it important? karna memberitahu kita valuenya true jika datanya valid
+  // how is it important? untuk memastikan fungsi akan keluar ketika data valid
   const handleSubmit = (e) => {
     e.preventDefault();
     if(values.firstName && values.lastName && values.email) {
@@ -57,24 +61,32 @@ export default function App() {
   };
 
   // =================
-
+  // what? state untuk fungsi yang berjalan SETELAH seluruh data inputan dinyatakan valid
+  // why is it important? untuk memastikan kolom inputan valid terisi
+  // how is it important? mencegah inputan disubmit dengan data kosong
   const [valid, setValid] = useState(false);
 
 
   return (
-    <div className="form-container">
+    <div className="container">
       <h1>Submit Data</h1>
-      <form className="register-form" onSubmit={handleSubmit}>
-        {submitted && valid ? <div class="success-message">Success! Thank you for registering</div> : null}
+      <form className="registerForm" onSubmit={handleSubmit}>
+        {submitted && valid ? <div class="successMessage">Success! Thank you for registering</div> : null}
+        {/*diatas ini, successMessage akan muncul jika data sukses disubmit dan data valid*/}
         <input
           className="boxInputan"
+          // diatas ini, classname boxInputan dipakai untuk seluruh box inputan + tombol submit karena stylingnya sama
           type="text"
+          // type text digunakan karena bentuk inputan yang diharapkan adalah teks
           placeholder="First Name"
+          // placeholder digunakan untuk memandu user untuk menginput data yang tepat pada kolom inputan
           value={values.firstName}
+          //value untuk menyambungkan inputan dengan tampungan yang dibuat di state diatas
           onChange={handleFirstNameInputChange}
+          // onChange dibuat untuk mengakomodir perubahan yang dilakukan di inputan
         />
-        {/* Uncomment the next line to show the error message */}
         {submitted && !values.firstName && <span id='first-name-error'>Please enter a first name</span>}
+        {/*diatas ini, first-name-error akan muncul jika data disubmit dan data tidak valid/kosong*/}
 
         <input
           className="boxInputan"
@@ -83,18 +95,20 @@ export default function App() {
           value={values.lastName}
           onChange={handleLastNameInputChange}
         />
-        {/* Uncomment the next line to show the error message */}
         {submitted && !values.lastName && <span id='last-name-error'>Please enter a last name</span>}
+        {/*diatas ini, last-name-error akan muncul jika data disubmit dan data tidak valid/kosong*/}
 
         <input
           className="boxInputan"
           type="email"
+          // type email digunakan karena bentuk inputan yang diharapkan adalah email
           placeholder="Email"
           value={values.email}
           onChange={handleEmailInputChange}
         />
-        {/* Uncomment the next line to show the error message */}
         {submitted && !values.email && <span id='email-error'>Please enter an email address</span>}
+        {/*diatas ini, email-error akan muncul jika data disubmit dan data tidak valid/kosong*/}
+
         <button className="boxInputan" type="submit">
           Submit
         </button>
